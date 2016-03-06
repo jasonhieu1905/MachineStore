f87032b4fab11fdc736dfbef430ec759be19f8b2
hieu doan <doanconghieu92@gmail.com>
2016-03-06 10:59:48 +0700

    -First commit
    	* Base framework
    
    Change-Id: I4757719038de70f98159a31aa51e52a8ece99a9f

---
+++ src/main/java/com/machine/controller/admin/AdminHomePage.java
@@ -0,0 +1,21 @@
+package com.machine.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class AdminHomePage {
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String hello(ModelMap model) {
+		model.addAttribute("name", "JCG Hello World!");
+		return "adminHomePage";
+
+	}
+	
+	
+}
