32cbebbcf0d5ba50ade128924dcec56e1f04f8c9
vpnocsen <vpnocsen@gmail.com>
2016-03-25 00:20:08 +0700

    @Nam Finish GUI

---
+++ src/main/java/com/machine/controller/user/ProductCatagoy.java
@@ -0,0 +1,62 @@
+package com.machine.controller.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.machine.model.Category;
+import com.machine.model.Product;
+import com.machine.service.CategoryService;
+import com.machine.service.ProductService;
+
+@Controller
+public class ProductCatagoy {
+	@Autowired
+	CategoryService categoryService;
+
+	@Autowired
+	ProductService productService;
+
+	@RequestMapping(value = "/category/{typeId}/{id}", method = RequestMethod.GET)
+	public String homePage(@PathVariable int typeId, @PathVariable int id, ModelMap model) {
+
+		List<Category> mainCatalogues = new ArrayList<>();
+		mainCatalogues = categoryService.getCategoriesMainProduct();
+
+		List<Category> accCatalogues = new ArrayList<>();
+		accCatalogues = categoryService.getCategoriesAccessories();
+
+		Category currentCategory = new Category();
+		String currentPage ="";
+		if (typeId == 1) {
+			currentPage = "product";
+			for (int i = 0; i < mainCatalogues.size(); i++) {
+				if (mainCatalogues.get(i).getId().equals(id)) {
+					currentCategory = mainCatalogues.get(i);
+					break;
+				}
+			}
+		} else if (typeId == 2) {
+			currentPage = "accessories";
+			for (int i = 0; i < accCatalogues.size(); i++) {
+				if (accCatalogues.get(i).getId().equals(id)) {
+					currentCategory = accCatalogues.get(i);
+					break;
+				}
+			}
+		}
+
+
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("catalogues", mainCatalogues);
+		model.addAttribute("accessories", accCatalogues);
+		model.addAttribute("currentCategory", currentCategory);
+		return "category";
+	}
+}
---
+++ src/main/java/com/machine/controller/user/ProductDetail.java
@@ -0,0 +1,67 @@
+package com.machine.controller.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.machine.model.Category;
+import com.machine.service.CategoryService;
+import com.machine.service.ProductService;
+
+@Controller
+@RequestMapping("/detail")
+public class ProductDetail {
+	
+	@Autowired
+	CategoryService categoryService;
+
+	@Autowired
+	ProductService productService;
+	
+	@RequestMapping(value="/{typeId}/{categoryId}/{productId}", method = RequestMethod.GET)
+	public String getProductDetail(@PathVariable int typeId, @PathVariable int categoryId,@PathVariable int productId,ModelMap model) {
+
+		List<Category> mainCatalogues = new ArrayList<>();
+		mainCatalogues = categoryService.getCategoriesMainProduct();
+		
+		List<Category> accCatalogues = new ArrayList<>();
+		accCatalogues = categoryService.getCategoriesAccessories();
+		
+		com.machine.model.Product product = productService.getProductById(productId);
+		String[] imageList = product.getZoomImage().split(",");
+		
+		Category currentCategory = new Category();
+		String currentPage = "";
+		if(typeId == 1){
+			currentPage = "product";
+			for(int i = 0;i<mainCatalogues.size();i++){
+				if(mainCatalogues.get(i).getId().equals(categoryId)){
+					currentCategory = mainCatalogues.get(i);
+				}
+			}
+		}else if(typeId == 2){
+			currentPage = "accessories";
+			for(int i = 0;i<accCatalogues.size();i++){
+				if(accCatalogues.get(i).getId().equals(categoryId)){
+					currentCategory = accCatalogues.get(i);
+				}
+			}
+		}
+		
+		model.addAttribute("currentPage",currentPage);
+		model.addAttribute("catalogues", mainCatalogues);
+		model.addAttribute("accessories", accCatalogues);
+		model.addAttribute("product",product);
+		model.addAttribute("imageList",imageList);
+		model.addAttribute("currentCategory",currentCategory);
+		
+		return "detail";
+
+	}
+}
